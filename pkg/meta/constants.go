c2d31ffd8404c4de0b0f7599c6e4e4b265398e5b
Leonardo Di Donato <leodidonato@gmail.com>
2019-05-19 19:59:19 -0700

    new: meta stuff
    Co-authored-by: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ pkg/meta/constants.go
@@ -0,0 +1,12 @@
+package meta
+
+const (
+	// DigPrefix is the prefix to identify objects created by this tool
+	DigPrefix = "kubectl-dig-"
+	// DigIDLabelKey is a meta to annotate objects created by this tool
+	DigIDLabelKey = "iovisor.org/kubectl-dig-id"
+	// DigLabelKey is a meta to annotate objects created by this tool
+	DigLabelKey = "iovisor.org/kubectl-dig"
+	// ObjectNamePrefix is the prefix used for objects created by this tool
+	ObjectNamePrefix = "kubectl-dig"
+)
